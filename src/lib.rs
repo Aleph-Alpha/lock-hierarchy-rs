f796f6b91b3cd0e36f1a66458f6d386d8eb479d3
Julian Wiesler <julian.wiesler@ext.aleph-alpha.com>
2025-02-10 12:15:52 +0100

    refactor: Mutex to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,11 @@
 //! This crate offers debug assertions for violations of lock hierarchies. No runtime overhead or
 //! protection occurs for release builds.
+//!
+//! Each lock is assigned a level. Locks with higher levels must be acquired before locks with
+//! lower levels.
+//! Both [RwLock] and [Mutex] use the same hierarchy.
 
 mod level;
+mod mutex;
 
-use std::sync::LockResult;
-
-use crate::level::{Level, LevelGuard};
-use std::{
-    ops::{Deref, DerefMut},
-    sync::PoisonError,
-};
-
-/// Wrapper around a [`std::sync::Mutex`] which uses a thread local variable in order to check for
-/// lock hierachy violations in debug builds.
-///
-/// Each Mutex is assigned a level. Mutexes with higher levels must be acquired before mutexes  with
-/// lower levels.
-///
-/// ```
-/// use lock_hierarchy::Mutex;
-///
-/// let mutex_a = Mutex::new(()); // Level 0
-/// let mutex_b = Mutex::with_level((), 0); // also level 0
-/// // Fine, first mutex in thread
-/// let _guard_a = mutex_a.lock().unwrap();
-/// // Would panic, lock hierarchy violation
-/// // let _guard_b = mutex_b.lock().unwrap();
-/// ```
-#[derive(Debug, Default)]
-pub struct Mutex<T> {
-    level: Level,
-    inner: std::sync::Mutex<T>,
-}
-
-impl<T> Mutex<T> {
-    /// Creates Mutex with level 0. Use this constructor if you want to get an error in debug builds
-    /// every time you acquire another mutex while holding this one.
-    pub fn new(t: T) -> Self {
-        Self::with_level(t, 0)
-    }
-
-    /// Creates a mutex and assigns it a level in the lock hierarchy. Higher levels must be acquired
-    /// first if locks are to be held simultaniously. This way we can ensure locks are always
-    /// acquired in the same order. This prevents deadlocks.
-    pub fn with_level(t: T, level: u32) -> Self {
-        Mutex {
-            level: Level::new(level),
-            inner: std::sync::Mutex::new(t),
-        }
-    }
-
-    /// See [std::sync::Mutex::lock]
-    pub fn lock(&self) -> Result<MutexGuard<T>, PoisonError<std::sync::MutexGuard<'_, T>>> {
-        self.inner.lock().map(|guard| MutexGuard {
-            inner: guard,
-            _level: self.level.lock(),
-        })
-    }
-
-    /// See [std::sync::Mutex::get_mut]
-    pub fn get_mut(&mut self) -> LockResult<&mut T> {
-        // No need to check hierarchy, this does not lock
-        self.inner.get_mut()
-    }
-}
-
-impl<T> From<T> for Mutex<T> {
-    /// Creates a new mutex in an unlocked state ready for use.
-    /// This is equivalent to [`Mutex::new`].
-    fn from(value: T) -> Self {
-        Mutex::new(value)
-    }
-}
-
-pub struct MutexGuard<'a, T> {
-    inner: std::sync::MutexGuard<'a, T>,
-    _level: LevelGuard,
-}
-
-impl<T> Deref for MutexGuard<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.inner.deref()
-    }
-}
-
-impl<T> DerefMut for MutexGuard<'_, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.inner.deref_mut()
-    }
-}
+pub use mutex::{Mutex, MutexGuard};
---
+++ src/mutex.rs
@@ -0,0 +1,87 @@
+use std::{
+    ops::{Deref, DerefMut},
+    sync::{LockResult, PoisonError},
+};
+
+use crate::level::{Level, LevelGuard};
+
+/// Wrapper around a [`std::sync::Mutex`] which uses a thread local variable in order to check for
+/// lock hierarchy violations in debug builds.
+///
+/// See the [crate level documentation](crate) for more general information.
+///
+/// ```
+/// use lock_hierarchy::Mutex;
+///
+/// let mutex_a = Mutex::new(()); // Level 0
+/// let mutex_b = Mutex::with_level((), 0); // also level 0
+/// // Fine, first mutex in thread
+/// let _guard_a = mutex_a.lock().unwrap();
+/// // Would panic, lock hierarchy violation
+/// // let _guard_b = mutex_b.lock().unwrap();
+/// ```
+#[derive(Debug, Default)]
+pub struct Mutex<T> {
+    inner: std::sync::Mutex<T>,
+    level: Level,
+}
+
+impl<T> Mutex<T> {
+    /// Creates lock with level 0. Use this constructor if you want to get an error in debug builds
+    /// every time you acquire another lock while holding this one.
+    pub fn new(t: T) -> Self {
+        Self::with_level(t, 0)
+    }
+
+    /// Creates a lock and assigns it a level in the lock hierarchy. Higher levels must be acquired
+    /// first if locks are to be held simultaneously. This way we can ensure locks are always
+    /// acquired in the same order. This prevents deadlocks.
+    pub fn with_level(t: T, level: u32) -> Self {
+        Mutex {
+            inner: std::sync::Mutex::new(t),
+            level: Level::new(level),
+        }
+    }
+
+    /// See [std::sync::Mutex::lock]
+    pub fn lock(&self) -> Result<MutexGuard<T>, PoisonError<std::sync::MutexGuard<'_, T>>> {
+        let level = self.level.lock();
+        self.inner.lock().map(|guard| MutexGuard {
+            inner: guard,
+            _level: level,
+        })
+    }
+
+    /// See [std::sync::Mutex::get_mut]
+    pub fn get_mut(&mut self) -> LockResult<&mut T> {
+        // No need to check hierarchy, this does not lock
+        self.inner.get_mut()
+    }
+}
+
+impl<T> From<T> for Mutex<T> {
+    /// Creates a new mutex in an unlocked state ready for use.
+    /// This is equivalent to [`Mutex::new`].
+    fn from(value: T) -> Self {
+        Mutex::new(value)
+    }
+}
+
+pub struct MutexGuard<'a, T> {
+    inner: std::sync::MutexGuard<'a, T>,
+    _level: LevelGuard,
+}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.inner.deref()
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.deref_mut()
+    }
+}
